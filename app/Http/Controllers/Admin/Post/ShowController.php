0d70c0d4c9a6721ffaae3650d616f18271c556f4
local_work <local_work@laravel.ru>
2024-02-02 16:57:09 +0300

    Work_02.02.2024

---
+++ app/Http/Controllers/Admin/Post/ShowController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Post;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tag;
+
+class ShowController extends Controller
+{
+    public function __invoke(Post $post)
+    {
+        return view('admin.post.show',compact('post'));
+    }
+}
---
+++ resources/views/admin/post/show.blade.php
@@ -0,0 +1,75 @@
+@extends('admin.layouts.main')
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6 d-flex align-items-center">
+                        <h1 class="mr-10">
+                            Просмотр категории "{{$post->title}}"
+                        </h1>
+                        <a href="{{ route('admin.post.edit',$post->id) }}"
+                            <i class="fas fa-pencil-alt text-success">
+
+                            </i>
+                        </a>
+                        <form action="{{ route('admin.post.delete',$post->id) }}" method="POST">
+                            @csrf
+                            @method('delete')
+                            <button type='submit' class="fas fa-trash-alt border-0 bg-transparent text-danger"></button>
+                        </form>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v1</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-3">
+                        <div class="card">
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Название</th>
+                                    </tr>
+                                    </thead>
+
+                                    <tbody>
+
+                                        <tr>
+                                            <td>{{$post->id}}</td>
+                                            <td>{{$post->title}}</td>
+
+                                        </tr>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+
+                    </div>
+
+                    <!-- /.row -->
+                </div>
+
+            </div>
+
+        </section>
+
+        <!-- /.content -->
+    </div>
+@endsection
+
